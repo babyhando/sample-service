1cb26ecf562484ebd7957102b0e64f9dbf9d125b
dara.nasibi <daranasibi66@gmail.com>
2024-06-14 15:03:51 +0330

    add common error structure to handlers

---
+++ api/http/handlers/helpers.go
@@ -0,0 +1,12 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func sendError(c *fiber.Ctx, err error, status int) error {
+	if status == 0 {
+		status = fiber.StatusInternalServerError
+	}
+	return c.Status(status).JSON(map[string]any{
+		"error_msg": err.Error(),
+	})
+}
